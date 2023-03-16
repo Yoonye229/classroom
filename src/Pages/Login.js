692d73c09cc48c912a0dd8af2c653e2d4dbc3e4f
minh tran <binhminh220901@gmail.com>
2023-03-16 14:53:00 +0700

    fix APP , Login

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import * as React from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 import Avatar from '@mui/material/Avatar';
@@ -126,11 +125,4 @@ export default function SignInSide() {
       </Grid>
     </ThemeProvider>
   );
-=======
-import React from "react";
-function login(){
-    return(
-       <></>
-    );
->>>>>>> e8516f45fab7f3e599c5ac5a716ced1a8ee26d39
 }
